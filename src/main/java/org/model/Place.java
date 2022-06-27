a526d40778b3ada3714f13904db10f16f31fca64
Igor <igor-rh@hotmail.com>
2022-06-27 19:03:53 -0300

    Getting id for update on constructor

--- src/main/java/org/model/Place.java
+++ src/main/java/org/model/Place.java
@@ -1,5 +1,7 @@
 package org.model;
 
+import java.time.LocalDateTime;
+
 /*
  * A place must have the following fields:
  * - name
@@ -10,6 +12,11 @@ package org.model;
  * - updated at
  */
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
@@ -17,24 +24,28 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
  @Entity
  public class Place extends PanacheEntity {
-
-
+     
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private String name;
+    
     private String slug;
     private String city;
     private String state;
-    private String createdAt;
-    private String updatedAt;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp 
+    private LocalDateTime updatedAt;
 
     public Place() {
     }
 
-    public Place( String name, String slug, String city, String state) {
+    public Place( String name, String slug, String city, String state, Long... id) {
         this.name = name;
         this.slug = slug;
         this.city = city;
         this.state = state;
+        this.id = id[0];
     
     }
 
@@ -52,11 +63,40 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
     public String getState() {
         return state;
     }
-    public String getCreatedAt() {
+    public LocalDateTime getCreatedAt() {
         return createdAt;
     }
-    public String getUpdatedAt() {
+    public LocalDateTime getUpdatedAt() {
         return updatedAt;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Place [city=" + city + ", name=" + name + ", slug=" + slug + ", state=" + state + "]";
     }
 
     
