2a0cdacc1a4e878ccd9ece1d505fb14b0580b20d
Igor <igor-rh@hotmail.com>
2022-06-27 19:03:21 -0300

    Creating updatePlace method on PlaceDao

--- src/main/java/org/dao/PlaceDao.java
+++ src/main/java/org/dao/PlaceDao.java
@@ -14,4 +14,20 @@ public class PlaceDao {
         }
         else return null;
     }
+
+    public Place updatePlace(Place place) {
+
+        try {
+            Place newPlace = Place.findById(place.id);
+            newPlace.setName(place.getName());
+            newPlace.setSlug(place.getSlug());
+            newPlace.setCity(place.getCity());
+            newPlace.setState(place.getState());
+            newPlace.persist();
+            return newPlace;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        
+    }
 }
